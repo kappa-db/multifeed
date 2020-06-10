ce622846b999bce1d032d74e7aa79b83cd00252d
Franz Heinzmann (Frando) <frando@unbiskant.org>
2020-06-10 17:15:07 +0200

    Allow to use the mux extensions with corestore.
    Exposes a CorestoreMuxer that makes the mux extensions usable on top of
    corestore and corestore-swarm-networker.
    
    See test/corestore.js for test and example.

---
+++ corestore.js
@@ -0,0 +1,114 @@
+const crypto = require('hypercore-crypto')
+const Multiplexer = require('./mux')
+const { EventEmitter } = require('events')
+
+class CorestoreMuxerTopic extends EventEmitter {
+  constructor (corestore, rootKey, opts = {}) {
+    super()
+    this.corestore = corestore
+    this.rootKey = rootKey
+    this._feeds = new Map()
+    this.streams = new Map()
+  }
+
+  addStream (stream, info) {
+    const self = this
+    const isInitiator = !!info.client
+    const opts = { stream, live: true }
+
+    const mux = new Multiplexer(isInitiator, this.rootKey, opts)
+
+    mux.on('manifest', onmanifest)
+    mux.on('replicate', onreplicate)
+    mux.ready(onready)
+
+    stream.once('end', cleanup)
+    stream.once('error', cleanup)
+    this.streams.set(stream, { mux, cleanup })
+
+    function onready () {
+      const keys = Array.from(self._feeds.keys())
+      if (keys.length) mux.offerFeeds(keys)
+    }
+
+    function onmanifest (manifest) {
+      mux.requestFeeds(manifest.keys)
+    }
+
+    function onreplicate (keys, repl) {
+      for (const key of keys) {
+        if (self._feeds.has(key)) continue
+        self.addFeed(key)
+        self.emit('feed', self._feeds.get(key))
+      }
+      const feeds = keys.map(key => self._feeds.get(key))
+      repl(feeds)
+    }
+
+    function cleanup (_err) {
+      mux.removeListener('manifest', onmanifest)
+      mux.removeListener('replicate', onreplicate)
+      self.streams.delete(stream)
+    }
+  }
+
+  removeStream (stream) {
+    if (!this.streams.has(stream)) return
+    const { cleanup } = this.streams.get(stream)
+    cleanup()
+  }
+
+  feeds () {
+    return Array.from(this._feeds.values())
+  }
+
+  addFeed (key) {
+    if (!Buffer.isBuffer(key)) key = Buffer.from(key, 'hex')
+    const feed = this.corestore.get(key)
+    const hkey = feed.key.toString('hex')
+    this._feeds.set(hkey, feed)
+    for (const { mux } of this.streams.values()) {
+      if (mux.knownFeeds().indexOf(hkey) === -1) {
+        // debug('Forwarding new feed to existing peer:', hexKey)
+        mux.offerFeeds([hkey])
+      }
+    }
+  }
+}
+
+module.exports = class CorestoreMuxer {
+  constructor (networker) {
+    this.networker = networker
+    this.corestore = networker.corestore
+    this.muxers = new Map()
+
+    this._joinListener = this._onjoin.bind(this)
+    this._leaveListener = this._onleave.bind(this)
+    this.networker.on('handshake', this._joinListener)
+    this.networker.on('stream-closed', this._leaveListener)
+  }
+
+  _onjoin (stream, info) {
+    for (const mux of this.muxers.values()) {
+      mux.addStream(stream, info)
+    }
+  }
+
+  _onleave (stream, info, finishedHandshake) {
+    for (const mux of this.muxers.values()) {
+      mux.removeStream(stream)
+    }
+  }
+
+  join (rootKey, opts) {
+    if (!Buffer.isBuffer(rootKey)) rootKey = Buffer.from(rootKey, 'hex')
+    const hkey = rootKey.toString('hex')
+    if (this.muxers.has(hkey)) return this.muxers.get(hkey)
+
+    const discoveryKey = crypto.discoveryKey(rootKey)
+    const mux = new CorestoreMuxerTopic(this.corestore, rootKey, opts)
+    this.networker.join(discoveryKey)
+    this.muxers.set(hkey, mux)
+    return mux
+  }
+}
---
+++ test/corestore.js
@@ -0,0 +1,171 @@
+const test = require('tape')
+const crypto = require('hypercore-crypto')
+const hyperswarm = require('hyperswarm')
+const ram = require('random-access-memory')
+const Corestore = require('corestore')
+const dht = require('@hyperswarm/dht')
+const SwarmNetworker = require('corestore-swarm-networking')
+
+const Muxer = require('../corestore')
+const multifeed = require('..')
+
+const BOOTSTRAP_PORT = 3100
+var bootstrap = null
+
+const KEY_A = Buffer.alloc(32, 1)
+const KEY_B = Buffer.alloc(32, 2)
+
+test('corestore networker example', async function (t) {
+  const { store: store1, networker: networker1 } = await create()
+  const { store: store2, networker: networker2 } = await create()
+
+  const core1 = store1.get()
+  const core2a = store2.get()
+  const core2b = store2.get()
+
+  await append(core1, 'hello')
+  const data = await get(core1, 0)
+  t.same(data, Buffer.from('hello'))
+
+  const muxer1 = new Muxer(networker1)
+  const muxer2 = new Muxer(networker2)
+
+  const mux1a = muxer1.join(KEY_A, { name: 'm1a' })
+  const mux1b = muxer1.join(KEY_B, { name: 'm1b' })
+
+  const mux2a = muxer2.join(KEY_A, { name: 'mux2a' })
+  const mux2b = muxer2.join(KEY_B, { name: 'mux2b' })
+
+  mux1a.addFeed(core1.key)
+  mux1b.addFeed(core1.key)
+
+  mux2a.addFeed(core2a.key)
+  mux2b.addFeed(core2b.key)
+
+  // TODO: Remove timeout, wait for event instead.
+  await timeout(500)
+
+  t.deepEqual(toKeys(mux1a.feeds()), toKeys([core1, core2a]))
+  t.deepEqual(toKeys(mux2a.feeds()), toKeys([core1, core2a]))
+  t.deepEqual(toKeys(mux1b.feeds()), toKeys([core1, core2b]))
+  t.deepEqual(toKeys(mux2b.feeds()), toKeys([core1, core2b]))
+  let checked = false
+  for (const feed of mux2b.feeds()) {
+    if (feed.key.toString('hex') === core1.key.toString('hex')) {
+      const data = await get(store2.get(core1.key), 0)
+      t.same(data, Buffer.from('hello'))
+      checked = true
+    }
+  }
+  if (!checked) t.fail('missing data check')
+  await cleanup([networker1, networker2])
+  t.end()
+})
+
+test('corestore to multifeed over hyperswarm', async t => {
+  const muxkey = KEY_A
+
+  // setup a corestore muxer
+  const { store, networker } = await create()
+  const core = store.get()
+  const muxer = new Muxer(networker)
+  const mux = muxer.join(muxkey)
+  mux.addFeed(core.key)
+
+  // setup a multifeed muxer plus network
+  const multi = multifeed(ram, { encryptionKey: muxkey })
+  let multifeedWriter
+  await new Promise(resolve => {
+    multi.writer('local', (err, feed) => {
+      multifeedWriter = feed
+      t.error(err)
+      feed.append('hello', resolve)
+    })
+  })
+  const swarm2 = hyperswarm({
+    bootstrap: `localhost:${BOOTSTRAP_PORT}`
+  })
+  swarm2.join(crypto.discoveryKey(muxkey), { announce: true, lookup: true })
+  let didConnect = false
+  // Just pipe all connections directly into the multifeed replication stream.
+  // This is what e.g. cabal currently does, and would need a seperate hyperswarm
+  // instance to replicate multiple multifeeds in parallel (before this patch).
+  swarm2.on('connection', (socket, details) => {
+    if (didConnect) return
+    didConnect = true
+    const isInitiator = !!details.client
+    const stream = multi.replicate(isInitiator, { live: true })
+    stream.pipe(socket).pipe(stream)
+  })
+
+  // wait and see
+  await timeout(500)
+  t.deepEqual(toKeys(multi.feeds()), toKeys([multifeedWriter, core]))
+  t.deepEqual(toKeys(mux.feeds()), toKeys([multifeedWriter, core]))
+  swarm2.destroy()
+  await cleanup([networker])
+})
+
+function toKeys (feeds) {
+  return feeds.map(f => f.key.toString('hex')).sort()
+}
+
+function append (core, data) {
+  return new Promise((resolve, reject) => {
+    core.append(data, err => {
+      if (err) return reject(err)
+      return resolve()
+    })
+  })
+}
+function get (core, idx, opts = {}) {
+  return new Promise((resolve, reject) => {
+    core.get(idx, opts, (err, data) => {
+      if (err) return reject(err)
+      return resolve(data)
+    })
+  })
+}
+
+function timeout (ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+async function create (opts = {}) {
+  if (!bootstrap) {
+    bootstrap = dht({
+      bootstrap: false
+    })
+    bootstrap.listen(BOOTSTRAP_PORT)
+    await new Promise(resolve => {
+      return bootstrap.once('listening', resolve)
+    })
+  }
+  const store = new Corestore(ram)
+  await store.ready()
+  const networker = new SwarmNetworker(store, {
+    ...opts,
+    bootstrap: `localhost:${BOOTSTRAP_PORT}`
+  })
+  // logEvents(networker, 'networker')
+  return { store, networker }
+}
+
+async function cleanup (networkers) {
+  for (let networker of networkers) {
+    await networker.close()
+  }
+  if (bootstrap) {
+    await bootstrap.destroy()
+    bootstrap = null
+  }
+}
+
+function logEvents (emitter, name) {
+  const emit = emitter.emit.bind(emitter)
+  emitter.emit = function (event, ...args) {
+    console.log(name, event)
+    if (event === 'replication-error') console.log(args)
+    emit(event, ...args)
+  }
+}
